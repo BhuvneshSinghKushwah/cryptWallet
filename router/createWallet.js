f64cf7264e9e4dcd666033fa5bcf5c2cface2ba6
Bhuvnesh Singh Kushwah <bhuvnesh049@gmail.com>
2023-12-25 12:19:18 +0530

    unique address creation logic added

--- router/createWallet.js
+++ router/createWallet.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bitcoin = require('bitcoinjs-lib');
 const {BIP32Factory} = require('bip32')
 const ecc = require('tiny-secp256k1')
-const bip32 = BIP32Factory(ecc)
+const bip32 = BIP32Factory(ecc);
 const bip39 = require('bip39');
 
 const Wallet = require('../model/wallets');
@@ -25,7 +25,35 @@ const addToMongoose = (phrase, address, pubKey, prvKey, blockchain) => {
     } catch (err) {
         console.error(err);
     }
-}
+};
+
+const createWallet = async (mnemonic, derivationCoin, idx) => {
+    let uniqueAddress = false;
+
+    while(!uniqueAddress)
+    {
+        try {
+            const derivationPath = `m/${derivationCoin}'/1'/0'/0/${idx}`
+            const seed = await bip39.mnemonicToSeed(mnemonic);
+            const root = bip32.fromSeed(seed);
+            const child = root.derivePath(derivationPath);
+            
+            const privateKey = child.toWIF();
+    
+            const { address } = bitcoin.payments.p2pkh({ pubkey: child.publicKey });
+            const publicKey = child.publicKey.toString('hex');
+
+            const existingWallet = await Wallet.findOne({address: address});
+            if(!existingWallet)
+            {
+                return {address, privateKey, publicKey};
+            }
+        } catch (err) {
+            console.error(err)
+        }
+        idx++;
+    }
+};
 
 router.post('/:blockchain', async (req, res) => {
     const mnemonic = req.body.phrase;
@@ -34,13 +62,10 @@ router.post('/:blockchain', async (req, res) => {
 
     const derivationPath = `m/${derivationCoin}'/1'/0'/0/0`
     try {
-        const seed = await bip39.mnemonicToSeed(mnemonic);
-        const root = bip32.fromSeed(seed);
-        const child = root.derivePath(derivationPath);
-        const privateKey = child.toWIF();
-
-        const { address } = bitcoin.payments.p2pkh({ pubkey: child.publicKey });
-        const publicKey = child.publicKey.toString('hex');
+        const obj = await createWallet(mnemonic, derivationCoin, 0);
+        const address = obj.address;
+        const publicKey = obj.publicKey;
+        const privateKey = obj.privateKey;
 
         await addToMongoose(mnemonic, address, publicKey, privateKey, blockchain);
 
